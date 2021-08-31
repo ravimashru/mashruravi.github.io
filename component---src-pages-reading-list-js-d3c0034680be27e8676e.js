b7180a508e858bbaab6f69efc1797b65979b1ab1
ravimashru <me@ravimashru.dev>
2021-08-31 18:32:30 +0530

    Updates

--- component---src-pages-reading-list-js-d3c0034680be27e8676e.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{ng1e:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),i=n("Wbzz"),r=n("Bl7J"),o=n("vrFN");t.default=function(){return l.a.createElement(r.a,null,l.a.createElement(o.a,{title:"Reading List"}),l.a.createElement("h1",null,"Hi from the reading list"),l.a.createElement("p",null,"Welcome to my reading list"),l.a.createElement(i.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-reading-list-js-d3c0034680be27e8676e.js.map
--- component---src-pages-reading-list-js-db3d6ec8e1f9312d42f0.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{228:function(e,t,n){"use strict";n.r(t);var a=n(0),l=n.n(a),i=n(47),o=n(225),r=n(224);t.default=function(){return l.a.createElement(o.a,null,l.a.createElement(r.a,{title:"Reading List"}),l.a.createElement("h1",null,"Hi from the reading list"),l.a.createElement("p",null,"Welcome to my reading list"),l.a.createElement(i.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-reading-list-js-db3d6ec8e1f9312d42f0.js.map
